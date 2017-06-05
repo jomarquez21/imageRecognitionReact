dd7c4e50a52f5192a16ead5fbb22a5c44cddd82e
Jose Enrique Marquez <jmrqz21@gmail.com>
2017-06-04 20:54:23 -0400

    agregando conexion con firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+const config = {
+  apiKey: "AIzaSyB-InvWSoEVKx9wA5UY_P5Yuj4BYWKlgio",
+  authDomain: "imagerecognitionreact.firebaseapp.com",
+  databaseURL: "https://imagerecognitionreact.firebaseio.com",
+  projectId: "imagerecognitionreact",
+  storageBucket: "imagerecognitionreact.appspot.com",
+  messagingSenderId: "20005783564"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
